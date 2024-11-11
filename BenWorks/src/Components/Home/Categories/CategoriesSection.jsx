ff2936daa98a751f42df6ffa00bde3f9b97ddd63
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-11-11 22:48:50 +0200

    Categories hover

--- BenWorks/src/Components/Home/Categories/CategoriesSection.jsx
+++ BenWorks/src/Components/Home/Categories/CategoriesSection.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
+import React from "react";
 
 export default function CategoriesSection() {
   return (
     <section className="categories_container">
-      <div className='categorie'>
-        <div className='categorie_bubble'></div>
-        <a href="">Chairs</a>
+          <div className="category">
+        <div className="category_bubble"></div>
+        <div className="category_name_container">
+          <a className="category_name" href="">
+            Enviroment
+          </a>
+          <div className="category_name_line"></div>
+        </div>
       </div>
-      <div className='categorie'>
-        <div className='categorie_bubble'></div>
-        <a href="">Chairs</a>
+      <div className="category">
+        <div className="category_bubble"></div>
+        <div className="category_name_container">
+          <a className="category_name" href="">
+            Chairs
+          </a>
+          <div className="category_name_line"></div>
+        </div>
       </div>
-      <div className='categorie'>
-        <div className='categorie_bubble'></div>
-        <a href="">Chairs</a>
+      <div className="category">
+        <div className="category_bubble"></div>
+        <div className="category_name_container">
+          <a className="category_name" href="">
+            Chairs
+          </a>
+          <div className="category_name_line"></div>
+        </div>
       </div>
     </section>
-  )
+  );
 }
