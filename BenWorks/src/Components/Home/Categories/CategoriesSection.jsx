1cf389fe892907d16989fd709b62d224d65b05fe
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-11-11 16:19:25 +0200

    categories start

---
+++ BenWorks/src/Components/Home/Categories/CategoriesSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function CategoriesSection() {
+  return (
+    <section className="categories_container">
+      <div className='categorie'>
+        <div className='categorie_bubble'></div>
+        <a href="">Chairs</a>
+      </div>
+      <div className='categorie'>
+        <div className='categorie_bubble'></div>
+        <a href="">Chairs</a>
+      </div>
+      <div className='categorie'>
+        <div className='categorie_bubble'></div>
+        <a href="">Chairs</a>
+      </div>
+    </section>
+  )
+}
--- BenWorks/src/Components/Home/HomeLayout.jsx
+++ BenWorks/src/Components/Home/HomeLayout.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Banner from "./Banner/Banner.jsx";
+import CategoriesSection from "./Categories/CategoriesSection.jsx";
 
 export default function HomeLayout() {
-  return <Banner />;
+  return (
+    <>
+      <Banner />
+      <CategoriesSection />
+    </>
+  );
 }
