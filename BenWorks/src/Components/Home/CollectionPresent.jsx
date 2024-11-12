ca428016914d4d3e76dd56f7fa3d5584985708e3
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-11-12 13:55:35 +0200

    media screen

--- BenWorks/src/Components/Home/CollectionPresent.jsx
+++ BenWorks/src/Components/Home/CollectionPresent.jsx
@@ -1,7 +1,22 @@
-
+import bucks from "../../assets/Bucks.jpg";
 
 export default function CollectionPresent() {
   return (
-    <div className="collection_present_container">CollectionPresent</div>
-  )
+    <section className="collection_present_container">
+      <div className="collection_present_green_field">
+        <h2>Natural wood brings life to your home</h2>
+        <p>
+          In our assortment you can find different type of furnitures used for
+          inside and outside house.
+        </p>
+        <p className="collection_present_smallertext">
+          For any custom wishes, contact us and we will discuss how can
+          we realise it
+        </p>
+      </div>
+      <div className="collection_present_img_container">
+        <img src={bucks} alt="" />
+      </div>
+    </section>
+  );
 }
