6214e9adf1f8c90b01ce6cdd76e9513e43795d35
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-11-15 14:39:22 +0200

    footer social media done

--- BenWorks/src/Components/Common/Footer/Footer.jsx
+++ BenWorks/src/Components/Common/Footer/Footer.jsx
@@ -1,3 +1,6 @@
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer className="section_footer">
@@ -5,11 +8,37 @@ export default function Footer() {
         <h3>Call us </h3>
         <span>Available on Monday - Sunday from 5:00 PM. </span>
         <a href="tel:+473337890" className="call_container_phone">
-          +1234567890
+          <strong>+1234567890 </strong>
         </a>
       </div>
-      <div className="footer_containers"></div>
-      <div className="footer_containers"></div>
+      <div className="footer_containers mail_container">
+        <h3>Mail us </h3>
+        <span>We will answer within 1 working day. </span>
+        <a href="mailto: Theme@xclusiveliving.nl">
+          <strong>Theme@xclusiveliving.nl </strong>
+        </a>
+      </div>
+      <div className="footer_containers social_container">
+        <h3>Social Media</h3>
+        <ul className="social_media_list">
+          <li className="social_icons">
+            <a href="https://www.facebook.com/" blank>
+              <FaFacebook />
+            </a>
+          </li>
+          <li className="social_icons">
+            <a href="">
+              <FaInstagram />
+            </a>
+          </li>
+          <li className="social_icons">
+            <a href="">
+              <FaTwitter />
+            </a>
+          </li>
+        </ul>
+        <div></div>
+      </div>
     </footer>
   );
 }
