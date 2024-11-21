bcfac86df483f7acebd2252a8d95071564719809
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-11-21 11:47:29 +0200

    footer animation done

--- BenWorks/src/Components/Common/Footer/Footer.jsx
+++ BenWorks/src/Components/Common/Footer/Footer.jsx
@@ -6,25 +6,48 @@ import { useScroll, animated } from "@react-spring/web";
 export default function Footer() {
   const { scrollYProgress } = useScroll();
 
-
-
   return (
     <footer className="section_footer">
+      <animated.div
+        className="footer_containers call_container"
+        style={{
+          position: "relative",
 
-      <div className="footer_containers call_container">
+          opacity: scrollYProgress.to(
+            (op) => `${parseFloat(op).toFixed(2) * 10 - 9}`
+          ),
+          right: scrollYProgress.to(
+            (val) => `${2000 - (val * 2000).toFixed()}px`
+          ),
+        }}
+      >
         <h3>Call us </h3>
         <span>Available on Monday - Sunday from 5:00 PM. </span>
         <a href="tel:+473337890" className="call_container_phone">
           <strong>+1234567890 </strong>
         </a>
-      </div>
-      <div className="footer_containers mail_container">
+      </animated.div>
+      <animated.div
+        className="footer_containers mail_container"
+        style={{
+          position: "relative",
+          visibility: scrollYProgress.to(
+            (op) => `${console.log(parseFloat(op).toFixed(2) * 10 - 9)}`
+          ),
+          opacity: scrollYProgress.to(
+            (op) => `${parseFloat(op).toFixed(2) * 10 - 9}`
+          ),
+          // top: scrollYProgress.to(
+          //   (val) => `${2000 - (val * 2000).toFixed()}px`
+          // ),
+        }}
+      >
         <h3>Mail us </h3>
         <span>We will answer within 1 working day. </span>
         <a href="mailto: Theme@xclusiveliving.nl">
           <strong>Theme@xclusiveliving.nl </strong>
         </a>
-      </div>
+      </animated.div>
       <animated.div
         className="footer_containers social_container"
         style={{
